a8c456b1f742505f84ef37c02982d7942983d810
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2024-06-03 13:03:12 -0300

    update version

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,13 +3,15 @@ import './NavBar.css';
 export default function NavBar(){
     return(
         <nav className='navbar'>
-            <h1>Gabriel <strong>Neto</strong></h1>
+            <h1><a href="">GNS</a></h1>
             <div className='pages'>
-                <a href="/">Home</a>
-                <a href="/about">About me</a>
-                <a href="/info">Information</a>
+                <ul>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">GitHub</a></li>
+                </ul>
             </div>
-            <a href="/contact">Contact</a>
+            <a href="#" className='contact'><span>Contact</span></a>
         </nav>
     );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,35 @@
 import React from "react";
 import './Home.css'
 import NavBar from '../../Components/NavBar/NavBar'
+import logo from '../../assets/foto-rosto.png'
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkeding.png'
+import instagram from '../../assets/instagram.png'
 
 export default function Home() {
-    
     return(
         <div className="body">
             <NavBar/>
-            
+            <div className="content">
+                <section className="container">
+                    <div className="escrita">
+                        <div>
+                            <h3>Meu nome é Gabriel Neto e,</h3>
+                            <h2>SOU PROGRAMADOR.</h2>
+                            <p>Seja bem-vindo ao meu portifólio website!</p>
+                            <a><button>Saiba mais sobre mim!</button></a>
+                        </div>
+                        <div className="icones">
+                            <a href="#"><img src={github} alt="github"/></a>
+                            <a href="#"><img src={linkedin} alt="linkedin"/></a>
+                            <a href="#"><img src={instagram} alt="instagram"/></a>
+                        </div>
+                    </div>
+                </section>
+                <section className="foto">
+                    <img src={logo} alt="Minha foto"/>
+                </section> 
+            </div>
         </div>
     );
 }
