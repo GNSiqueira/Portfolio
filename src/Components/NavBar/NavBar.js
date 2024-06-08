a11f136c82d8068346c67124ba2b55551741779b
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2024-06-08 18:03:59 -0300

    Links as páginas

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,15 +3,15 @@ import './NavBar.css';
 export default function NavBar(){
     return(
         <nav className='navbar'>
-            <h1><a href="">GNS</a></h1>
+            <h1><a href="/">GNS</a></h1>
             <div className='pages'>
                 <ul>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">GitHub</a></li>
+                    <li><a href="/portifolio">Portfolio</a></li>
+                    <li><a href="/about">Sobre</a></li>
+                    <li><a href="/github">GitHub</a></li>
                 </ul>
             </div>
-            <a href="#" className='contact'><span>Contact</span></a>
+            <a href="https://wa.me/5517997822073" target='_blank' className='contact'><span>Contato</span></a>
         </nav>
     );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -16,11 +16,11 @@ export default function Home() {
                             <span>Olá, seja bem-vindo</span>
                             <h2><span>BACK-END  </span>   DEVELOPER</h2>
                             <p>Desenvolvo soluções de backend sob medida para impulsionar a performance e a eficiência de sua aplicação. Com expertise em tecnologias de ponta, transformo conceitos em códigos robustos e escaláveis, garantindo uma base sólida para o sucesso de seu projeto. Confie em minha experiência para levar sua empresa além do esperado no mundo digital.</p>
-                            <a href="#"><button className="botao">Entre em contato!</button></a>
+                            <a href="https://wa.me/5517997822073" target='_blank' ><button className="botao">Entre em contato!</button></a>
                             <div className="icones">
-                                <a href="#"><img src={github} alt="github"/></a>
-                                <a href="#"><img src={linkedin} alt="linkedin"/></a>
-                                <a href="#"><img src={instagram} alt="instagram"/></a>
+                                <a href="https://github.com/GNSiqueira" target='_blank'><img src={github} alt="github"/></a>
+                                <a href="https://www.linkedin.com/in/gabrielnetosiqueira/" target='_blank'><img src={linkedin} alt="linkedin"/></a>
+                                <a href="https://www.instagram.com/eu.gabrielneto/" target='_blank'><img src={instagram} alt="instagram"/></a>
                             </div>
                     </div>
                 </section>
