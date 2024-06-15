05d93d9426f467557709cea0b94cf20a72bc1815
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2024-06-15 12:21:00 -0300

    Update version

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -2,6 +2,7 @@ import React from "react";
 import './Portfolio.css';
 import NavBar from "../../Components/NavBar/NavBar";
 import ProjetoIntegrador from "../../assets/Portifolio/projetointegrador.png"
+import IconeProjetoIntegrador from "../../assets/Portifolio/projeto-intregrador-icone.png"
 
 export default function Portfolio() {
     return (
@@ -10,7 +11,9 @@ export default function Portfolio() {
             <main>
                <section>
                     <div className="card">
-                        <a href="/test"><img src={ProjetoIntegrador} alt=""/></a>
+                        <a href="/projeto"><img src={ProjetoIntegrador} alt="Projeto Integrador"/>
+                        </a>
+                        <img src={IconeProjetoIntegrador}/>
                     </div><div className="card">
                         <a href=""><img src="https://picsum.photos/800" alt=""/></a>
                     </div><div className="card">
